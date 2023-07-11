516849363fb06d97ef1ba557830f870f1d48c654
HelkaPL <helka223@gmail.com>
2023-07-11 13:50:21 +0200

    first few steps

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,30 +1,11 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1 class="title">Wielkie odliczanie</h1>
+    <div class="bottom">
+      <image class="image-left" src="../assets/logoR.png" alt="Rummikube Logo R"/>
+      <image class="image-center" alt="event banner" />
+      <image class="image-right" src="../assets/face.png" alt="rummi-logo-face"/>
+    </div>
   </div>
 </template>
 
@@ -39,18 +20,31 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .hello {
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    background: rgb(11,192,223);
+    background: linear-gradient(90deg, rgba(11,192,223,1) 0%, rgba(252,221,89,1) 100%);
+  }
+  .title {
+    font-family: 'Emblema One', cursive;
+    text-transform: uppercase;
+    font-size: 48px;
+    color: red;
+  }
+  .time {
+    width: max-content;
+  }
+  .timer__label {
+    display: flex;
+    justify-content: space-evenly;
+  }
+  
 </style>
